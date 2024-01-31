412220f37f03b0d87edaa060aa0dcb27c51f2f84
Sakshi Tomar <98844582+Sakshitom@users.noreply.github.com>
2024-02-01 00:36:14 +0530

    Update Upload.js

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -44,7 +44,7 @@ const Upload = () => {
               id: row.id || "",
               links: row.links || "",
               prefix: row.prefix || "",
-              tags: [],
+              tags : [],
             }))
           );
           setTags(new Array(jsonData.length).fill([]));
@@ -69,7 +69,7 @@ const Upload = () => {
           id: row.id || "",
           links: row.links || "",
           prefix: row.prefix || "",
-          tags: [],
+          tags : [],
         }));
 
         setUploadedFiles(parsedData);
@@ -105,29 +105,29 @@ const Upload = () => {
     const updatedFiles = [...uploadedFiles];
     updatedFiles[fileIndex].tags.splice(tagIndex, 1);
     setUploadedFiles(updatedFiles);
-
+  
     const updatedTags = [...tags];
     updatedTags[fileIndex] = updatedFiles[fileIndex].tags;
     setTags(updatedTags);
   };
-
+  
   const handleDropdownChange = (index, event) => {
     const newTag = event.target.value;
-
+  
     // Ensure that the selected tag is not an empty string
     if (newTag.trim() !== "") {
       const updatedFiles = [...uploadedFiles];
       const existingTags = updatedFiles[index].tags || [];
-
+      
       // Check if the tag is not already in the array
       if (!existingTags.includes(newTag)) {
         updatedFiles[index].tags = [...existingTags, newTag];
         setUploadedFiles(updatedFiles);
-
+  
         const updatedTags = [...tags];
         updatedTags[index] = updatedFiles[index].tags;
         setTags(updatedTags);
-
+  
         // Clear the selected tag in the dropdown
         const newTags = [...selectedDropdownTags];
         newTags[index] = "";
@@ -135,7 +135,7 @@ const Upload = () => {
       }
     }
   };
-
+  
   return (
     <div className="">
       {/* Mobile */}
@@ -152,7 +152,7 @@ const Upload = () => {
                 onDrop={handleDrop}
                 onDragOver={handleDragOver}
               >
-                <img src={Excel} alt="excel" className="p-4 mt-4" />
+                <img src={Excel} alt="excel" className="p-4 mt-4"/>
                 <div className="flex">
                   <p>
                     {selectedFiles.length > 0
@@ -169,7 +169,7 @@ const Upload = () => {
                 <input
                   type="file"
                   id="fileInput"
-                  accept=".xls, .xlsx"
+                  accept=".xls,.xlsx,.csv,.tsv"
                   onChange={handleFileChange}
                   style={{ display: "none" }}
                   multiple
@@ -195,95 +195,88 @@ const Upload = () => {
           </div>
 
           <div className="p-3 pb-4 overflow-auto">
-            {uploadedFiles.length > 0 && (
-              <div className="text-black text-xl font-semibold leading-loose">
-                Uploads
-              </div>
-            )}
-            {uploadedFiles.length > 0 && (
-              <div className="bg-gray-200 px-4 pb-4 rounded-md overflow-auto w-full">
-                {uploadedFiles.length > 0 && (
-                  <div className="w-full bg-gray-200 mt-8 py-4 whitespace-nowrap rounded-md">
-                    <table className="w-full">
-                      <thead>
-                        <tr className="grid grid-cols-5 gap-8">
-                          <th className="font-semibold">S.No.</th>
-                          <th className="font-semibold">Links</th>
-                          <th className="font-semibold">Prefix</th>
-                          <th className="font-semibold px-4 whitespace-nowrap">
-                            Add Tags
-                          </th>
-                          <th className="font-semibold mx-8">Selected Tags</th>
-                        </tr>
-                      </thead>
-                    </table>
-                  </div>
-                )}
-                <table className="w-full">
-                  <tbody>
-                    {uploadedFiles.map((file, index) => (
-                      <tr
-                        key={index}
-                        className="w-full bg-white mt-8 py-4 px-2 whitespace-nowrap rounded-md"
+  {uploadedFiles.length > 0 && (
+    <div className="text-black text-xl font-semibold leading-loose">
+      Uploads
+    </div>
+  )}
+  {uploadedFiles.length > 0 && (
+    <div className="bg-gray-200 px-4 pb-4 rounded-md overflow-auto w-full">
+      {uploadedFiles.length > 0 && (
+        <div className="w-full bg-gray-200 mt-8 py-4 whitespace-nowrap rounded-md">
+          <table className="w-full">
+            <thead>
+              <tr className="grid grid-cols-5 gap-8">
+                <th className="font-semibold">S.No.</th>
+                <th className="font-semibold">Links</th>
+                <th className="font-semibold">Prefix</th>
+                <th className="font-semibold px-4 whitespace-nowrap">Add Tags</th>
+                <th className="font-semibold mx-8">Selected Tags</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      )}
+      <table className="w-full">
+        <tbody>
+          {uploadedFiles.map((file, index) => (
+            <tr
+              key={index}
+              className="w-full bg-white mt-8 py-4 px-2 whitespace-nowrap rounded-md"
+            >
+              <td>{index + 1}</td>
+              <td className="w-50px px-3 whitespace-nowrap">
+                <a
+                  href={file.links}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-blue-500 text-sm whitespace-nowrap"
+                >
+                  {file.links}
+                </a>
+              </td>
+              <td className="px-3 text-sm whitespace-nowrap">{file.prefix}</td>
+              <td>
+                <select
+                  value={selectedDropdownTags[index] || ""}
+                  onChange={(event) => handleDropdownChange(index, event)}
+                  className="border border-gray-400 rounded-md px-2 py-1 whitespace-nowrap"
+                >
+                  <option value="" disabled>
+                    Select Tag
+                  </option>
+                  {tagsList.map((tag) => (
+                    <option key={tag} value={tag}>
+                      {tag}
+                    </option>
+                  ))}
+                </select>
+              </td>
+              <td className="flex flex-col gap-2 whitespace-nowrap">
+                {uploadedFiles[index].tags &&
+                  uploadedFiles[index].tags.map((tag, tagIndex) => (
+                    <div
+                      key={tagIndex}
+                      className="bg-blue-500 text-white px-2 py-1 rounded-md flex items-center"
+                    >
+                      {tag}
+                      <span
+                        className="ml-2 cursor-pointer"
+                        onClick={() => handleTagRemove(index, tagIndex)}
                       >
-                        <td>{index + 1}</td>
-                        <td className="w-50px px-3 whitespace-nowrap">
-                          <a
-                            href={file.links}
-                            target="_blank"
-                            rel="noreferrer"
-                            className="text-blue-500 text-sm whitespace-nowrap"
-                          >
-                            {file.links}
-                          </a>
-                        </td>
-                        <td className="px-3 text-sm whitespace-nowrap">
-                          {file.prefix}
-                        </td>
-                        <td>
-                          <select
-                            value={selectedDropdownTags[index] || ""}
-                            onChange={(event) =>
-                              handleDropdownChange(index, event)
-                            }
-                            className="border border-gray-400 rounded-md px-2 py-1 whitespace-nowrap"
-                          >
-                            <option value="" disabled>
-                              Select Tag
-                            </option>
-                            {tagsList.map((tag) => (
-                              <option key={tag} value={tag}>
-                                {tag}
-                              </option>
-                            ))}
-                          </select>
-                        </td>
-                        <td className="flex flex-col gap-2 whitespace-nowrap">
-                          {uploadedFiles[index].tags &&
-                            uploadedFiles[index].tags.map((tag, tagIndex) => (
-                              <div
-                                key={tagIndex}
-                                className="bg-blue-500 text-white px-2 py-1 rounded-md flex items-center"
-                              >
-                                {tag}
-                                <span
-                                  className="ml-2 cursor-pointer"
-                                  onClick={() =>
-                                    handleTagRemove(index, tagIndex)
-                                  }
-                                >
-                                  &times;
-                                </span>
-                              </div>
-                            ))}
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            )}
-          </div>
+                        &times;
+                      </span>
+                    </div>
+                  ))}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )}
+</div>
+
         </div>
       </div>
 
@@ -326,7 +319,7 @@ const Upload = () => {
                 <input
                   type="file"
                   id="fileInput"
-                  accept=".xls,.xlsx,.csv"
+                  //accept=".xls,.xlsx,.csv,.tsv"
                   onChange={handleFileChange}
                   style={{ display: "none" }}
                   multiple
